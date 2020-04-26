7ab597793c8a6ef603f6c1a0c4b3bbac0f9cee1b
caoxijuan <2357569721@qq.com>
2020-04-26 22:39:04 +0800

    提交

---
+++ js/house.js
@@ -0,0 +1,5 @@
+// $('.house-nav-left-bottom>li').hover(function() {
+//     $('.house-nav-left-bottom>li .room').css({ "background-position": "1px -121px" })
+// }, function() {
+//     $('.house-nav-left-bottom>li .room').css({ "background-position": "1px -121px" })
+// })
